320c4068edd3af972b9c5c57e4b221f91da4eb8e
laljiyadav <laljiempire@gmail.com>
2018-03-20 01:53:38 +0545

    This is Test

---
+++ app/src/main/java/main/erp/root/erp/main/erp/root/erp/activity/GitHubTest.java
@@ -0,0 +1,15 @@
+package main.erp.root.erp.main.erp.root.erp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import main.erp.root.erp.R;
+
+public class GitHubTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_hub_test);
+    }
+}
